b567645bc050ee0dc1102fe647ee444b8293227f
rafaelgomesdc <rafaelgomesdc0000@gmail.com>
2026-01-14 23:52:39 -0300

    Linkando funcoes

--- game/palavra.py
+++ game/palavra.py
@@ -1,4 +1,14 @@
+import random
+
 class Palavra:
     palavra = []
     categoria = ""
     popularidade = ""
+
+    def definir_palavra(self, ls):
+        i = random.randint(0, len(ls))
+        print(i)
+        p_data = ls[i].split("_")
+        self.palavra = p_data[0]
+        self.categoria = p_data[1]
+        self.popularidade = p_data[2]
