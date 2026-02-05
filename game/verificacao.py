64a3bd4db439e30f0e004f9bdef5efc1717f3c89
rafaelgomesdc <rafaelgomesdc0000@gmail.com>
2026-02-05 15:34:09 -0300

    acentuacao na tentativa da letra implementada

--- game/verificacao.py
+++ game/verificacao.py
@@ -26,84 +26,9 @@ def acentuacao(palavra):
 def Verificar_Letra(letra, p_tentativa, palavra):
     erro = True
 
-    for l in range(len(palavra)):
-        """
-        if letra == "a":
-            match palavra[l]:
-                case "á":
-                    p_tentativa[l] = palavra[l]
-                    erro = False
-                case "à":
-                    p_tentativa[l] = palavra[l]
-                    erro = False
-                case "ã":
-                    p_tentativa[l] = palavra[l]
-                    erro = False
-                case "â":
-                    p_tentativa[l] = palavra[l]
-                    erro = False
-                #case "a":
-                #    p_tentativa[l] = palavra[l]
-                #erro = False
-        elif letra == "e":
-            match palavra[l]:
-                case "é":
-                    p_tentativa[l] = palavra[l]
-                    erro = False
-                case "è":
-                    p_tentativa[l] = palavra[l]
-                    erro = False
-                case "ê":
-                    p_tentativa[l] = palavra[l]
-                    erro = False
-                #case "e":
-                #    p_tentativa[l] = palavra[l]
-                #erro = False
-        elif letra == "i":
-            match palavra[l]:
-                case "í":
-                    p_tentativa[l] = palavra[l]
-                    erro = False
-                case "ì":
-                    p_tentativa[l] = palavra[l]
-                    erro = False
-                case "î":
-                    p_tentativa[l] = palavra[l]
-                    erro = False
-                #case "i":
-                #    p_tentativa[l] = palavra[l]
-                #erro = False
-        elif letra == "o":
-            match palavra[l]:
-                case "ó":
-                    p_tentativa[l] = palavra[l]
-                    erro = False
-                case "ò":
-                    p_tentativa[l] = palavra[l]
-                    erro = False
-                case "õ":
-                    p_tentativa[l] = palavra[l]
-                    erro = False
-                case "ô":
-                    p_tentativa[l] = palavra[l]
-                    erro = False
-                #case "o":
-                #    p_tentativa[l] = palavra[l]
-                #erro = False
-        elif letra == "u":
-            match palavra[l]:
-                case "ú":
-                    p_tentativa[l] = palavra[l]
-                    erro = False
-                case "ù":
-                    p_tentativa[l] = palavra[l]
-                    erro = False
-                #case "u":
-                #    p_tentativa[l] = palavra[l]
-        """
-        
+    for l in range(len(palavra)):    
         if letra == acentuacao(palavra[l]):
-            p_tentativa[l] = letra
+            p_tentativa[l] = palavra[l]
             erro = False
         
         p_tentativa[0] = p_tentativa[0].upper()
